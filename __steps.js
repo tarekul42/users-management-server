eca259fb2227d979c6eec0ec449f8197a7a0e9aa
tarekul42 <tarekulrifat205@gmail.com>
2023-10-25 11:54:18 -0700

    users management server is done

---
+++ __steps.js
@@ -0,0 +1,9 @@
+/**
+ * 1. Create a post API on the server site
+ * 2. Client side send data via post
+ * 3. set fetch method inside the fetch options (second parameter)
+ * 4. options will have three things: method
+ * 5. options will have headers: {'content-type' : 'application/json'}
+ * 6. don't forget to send data by JSON.stringify in the body
+ * 7. On the server side: don't forget to use express.json() middlewere
+*/
